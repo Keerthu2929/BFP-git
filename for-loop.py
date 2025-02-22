29a996105f83a9f3e929820b86fa8cc6fef679c2
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-22 17:22:32 +0530

    for-loop file added

---
+++ for-loop.py
@@ -0,0 +1,16 @@
+# for loop ()
+li = [1,8,9,1,6,7,3,1,9,4,1,8,4,2,1,5,7,2,1,0,3]
+ans = 0
+for i in li:
+  if i == 1:
+    ans += 1
+print(ans)
+
+# or
+
+# li = [1,8,9,1,6,7,3,1,9,4,1,8,4,2,1,5,7,2,1,0,3]
+# ans = 0
+# for i in range(21):
+#   if li[i] == 1:
+#     ans += 1
+# print(ans)
