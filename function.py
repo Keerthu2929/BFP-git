83cabc59b82fd357c642d2f7150223cdebd83591
keerthu2929 <keerthanamuni29@gmail.com>
2025-02-22 19:46:56 +0530

    function.py file added

---
+++ function.py
@@ -0,0 +1,13 @@
+#function
+def table(n):
+  for i in range(1,10):
+    print(f"{n}x{i}={n*i}")
+table(2)
+
+
+#return
+def my_function(x):
+  return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
