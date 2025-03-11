31c59a998198c9bb8e0b734621c370cea37c1766
keerthu2929 <keerthanamuni29@gmail.com>
2025-03-12 00:19:42 +0530

    sample-weight3.py file added

---
+++ sample-weight3.py
@@ -0,0 +1,15 @@
+# type conversion
+birth_year = int(input("Birth year: "))
+age = 2002 - (birth_year)
+print(age)
+print(type(birth_year))
+print(type(age))
+
+
+# example:
+weight_lbs = float(input("Enter your weight (lbs): "))
+weight_kg = weight_lbs * 0.45
+print(weight_kg)
+
+
+
