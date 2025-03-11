9b56d4f23c7f170ac368cb7a913d76f157fd645f
keerthu2929 <keerthanamuni29@gmail.com>
2025-03-12 00:10:30 +0530

    sample-patient1.py file added

---
+++ sample-patient1.py
@@ -0,0 +1,10 @@
+# How to printing and assign the variable 
+Name = "John Smith"
+Age = 20
+patient = input("new patient or exit patient:")
+
+# how to string concatenation
+print("My name is "+ Name + " and My age is "+ str(Age) + " years old " + "and is a " + patient)
+
+# using F-string
+print(f"My name is {Name} and My age is {Age} years old and is a {patient} ")
