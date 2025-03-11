00a3ecd8fd2f6927cec28458b4b2e5a20767917b
keerthu2929 <keerthanamuni29@gmail.com>
2025-03-12 00:18:27 +0530

    sample-person2.py file added

---
+++ sample-person2.py
@@ -0,0 +1,9 @@
+# input type
+Name = input("Enter your name : ")
+Favorite_color = input("Enter your favorites color : ")
+print(f"{Name} like {Favorite_color}.")
+# print(f"My name is {Name}.\n My favorite color is {Favorite_color}.")
+
+
+
+
